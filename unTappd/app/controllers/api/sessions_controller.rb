fd0c6ee5e4f22acb7b786391e5de392115d053fd
julie <juliesbock@gmail.com>
2020-06-23 10:55:11 -0700

    setup

---
+++ unTappd/app/controllers/api/sessions_controller.rb
@@ -0,0 +1,16 @@
+class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+
+    if @user
+      login(@user)
+      render :show
+    else
+      flash.now[errors] = @user.errors.full_messages
+      render :show
+    end
+  end
+end
